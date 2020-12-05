c03faf5f6a16812b67f3d6adb3b9c4bb7a264438
Toru Takefusa <torutakefusa@gmail.com>
2020-12-05 20:56:24 +0900

    Add solution of optional exercise 1.

--- 02_UDPPinger/UDPPingerClient.py
+++ 02_UDPPinger/UDPPingerClient.py
@@ -8,6 +8,11 @@ clientSocket = socket(AF_INET, SOCK_DGRAM)
 clientSocket.settimeout(1)
 
 i = 1
+minimum = 100
+maximum = 0
+sum = 0
+succceed = 0
+
 while i < 11:
     try:
         start = datetime.now()
@@ -18,10 +23,19 @@ while i < 11:
 
         end = datetime.now()
         diff = (end - start).total_seconds()
-        diff.set_printptions(suppress = True)
+        minimum = min(minimum, diff)
+        maximum = max(maximum, diff)
+        sum += diff
+        succceed += 1
         print(diff)
     except timeout:
         print("Request timed out")
     finally:
         i = i + 1
+print("min: " + str(minimum))
+print("max " + str(maximum))
+average = sum / succceed
+print("average: " + str(average))
+loss = (10 - succceed) / 10 * 100
+print("packet loss rate: " + str(loss) + "%")
 clientSocket.close()
